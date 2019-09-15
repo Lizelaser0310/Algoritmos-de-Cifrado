077c346a2e30652068933d010078e42970222f6d
Lizelaser0310 <lizeth.laserna.27@unsch.edu.pe>
2019-09-15 09:08:40 -0500

    primer-commit

---
+++ Cifrado Transposicion.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    #mensaje a cifrar
+    myMessage = 'a quien madruga dios le ayuda'
+    #la llave que utilizaremos
+    myKey = 6
+    #llamando a la funcion que se encarga de cifrar
+    ciphertext = encryptMessage(myKey, myMessage)
+
+    # Imprime la cedena cifrada y usando  '|' para indicar el fin
+    #del mensaje cifrado
+    print(ciphertext + '|')
+
+#funcion para cifrar el mensaje
+def encryptMessage(key, message):
+#Cada cadena de texto en el mensaje cifrado representa una columna en la matriz
+    ciphertext = [''] * key
+
+    # recorremos las columnas
+    for col in range(key):
+        pointer = col
+
+        #Lee cada elemento de la columna
+        while pointer < len(message):
+            # Pone el caracter que encuentra en el indice de la matriz
+            ciphertext[col] += message[pointer]
+
+            #mueve el puntero al siguiente elemento de la columna
+            pointer += key
+
+    # Convertimos la lista en una cadena de texto y  lo retornamos
+    return ''.join(ciphertext)
+
+# si el nombre de la funcion es main la manda a ejecutar
+if __name__ == '__main__':
+    main()
